2cba44d40f26ce08c668d68ad49d8d518e4f5178
ByteBane <81875670+bytebane@users.noreply.github.com>
2022-07-09 22:39:41 +0530

    ElegantOTA_added

--- SmartHome_32.ino
+++ SmartHome_32.ino
@@ -1,8 +1,11 @@
-//Created by Reniyal on 13/06/2022 @09:06 PM ISTs
+// Created by Reniyal on 13/06/2022 @09:06 PM ISTs
 
 #include "WiFi.h"
 #include "EEPROM.h"
 #include "FirebaseESP32.h"
+#include "AsyncTCP.h"
+#include "ESPAsyncWebServer.h"
+#include "AsyncElegantOTA.h"
 
 #define LENGTH(x) (strlen(x) + 1)
 #define EEPROM_SIZE 200
@@ -16,49 +19,55 @@ unsigned long rst_millis;
 
 FirebaseData fbdo;
 
-int LED_R = 21;
-int LED_Y = 22;
-int LED_G = 23;
+AsyncWebServer server(80);
 
-int LED1 = 16;
-int LED2 = 17;
-int LED3 = 18;
-int LED4 = 19;
+int PIN_R = 21;
+int PIN_Y = 22;
+int PIN_G = 23;
 
-bool light1;
-bool light2;
-bool light3;
-bool light4;
+int PIN1 = 16;
+int PIN2 = 17;
+int PIN3 = 18;
+int PIN4 = 19;
 
-void setup() {
+bool switch1;
+bool switch2;
+bool switch3;
+bool switch4;
+
+void setup()
+{
 
   Serial.begin(115200);
 
   pinMode(WiFi_rst, INPUT);
 
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_Y, OUTPUT);
-  pinMode(LED_G, OUTPUT);
+  pinMode(PIN_R, OUTPUT);
+  pinMode(PIN_Y, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
 
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
+  pinMode(PIN1, OUTPUT);
+  pinMode(PIN2, OUTPUT);
+  pinMode(PIN3, OUTPUT);
+  pinMode(PIN4, OUTPUT);
 
-  digitalWrite(LED_R, HIGH);
-  digitalWrite(LED_G, HIGH);
-  digitalWrite(LED_Y, HIGH);
+  digitalWrite(PIN_R, HIGH);
+  digitalWrite(PIN_G, HIGH);
+  digitalWrite(PIN_Y, HIGH);
 
   // For Solid State Relay High=OFF
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, HIGH);
-  digitalWrite(LED4, HIGH);
+  digitalWrite(PIN1, HIGH);
+  digitalWrite(PIN2, HIGH);
+  digitalWrite(PIN3, HIGH);
+  digitalWrite(PIN4, HIGH);
 
-  if (!EEPROM.begin(EEPROM_SIZE)) {
+  if (!EEPROM.begin(EEPROM_SIZE))
+  {
     Serial.println("failed to init EEPROM");
     delay(1000);
-  } else {
+  }
+  else
+  {
     ssid = readStringFromFlash(0);
     Serial.print("SSID = ");
     Serial.println(ssid);
@@ -71,32 +80,34 @@ void setup() {
 
   delay(3500);
 
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
 
     WiFi.mode(WIFI_AP_STA);
     WiFi.beginSmartConfig();
 
     Serial.println("Waiting for SmartConfig.");
-    while (!WiFi.smartConfigDone()) {
-      digitalWrite(LED_G, LOW);
-      digitalWrite(LED_Y, LOW);
+    while (!WiFi.smartConfigDone())
+    {
+      digitalWrite(PIN_G, LOW);
+      digitalWrite(PIN_Y, LOW);
       Serial.print(".");
-      digitalWrite(LED_R, HIGH);
+      digitalWrite(PIN_R, HIGH);
       delay(500);
       Serial.print(".");
-      digitalWrite(LED_R, LOW);
+      digitalWrite(PIN_R, LOW);
       delay(500);
       Serial.print(".");
-      digitalWrite(LED_Y, HIGH);
+      digitalWrite(PIN_Y, HIGH);
       delay(500);
       Serial.print(".");
-      digitalWrite(LED_Y, LOW);
+      digitalWrite(PIN_Y, LOW);
       delay(500);
       Serial.print(".");
-      digitalWrite(LED_G, HIGH);
+      digitalWrite(PIN_G, HIGH);
       delay(500);
       Serial.print(".");
-      digitalWrite(LED_G, LOW);
+      digitalWrite(PIN_G, LOW);
       delay(500);
     }
 
@@ -104,10 +115,11 @@ void setup() {
     Serial.println("SmartConfig received.");
 
     Serial.println("Waiting for WiFi");
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
       Serial.print(".");
     }
-    wifiConnected();
+    wifiLED();
     Serial.println("WiFi Connected!");
 
     Serial.print("IP Address: ");
@@ -123,31 +135,25 @@ void setup() {
     writeStringToFlash(ssid.c_str(), 0);
     writeStringToFlash(pass.c_str(), 40);
 
-
-    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-    Firebase.reconnectWiFi(true);
-    Firebase.setReadTimeout(fbdo, 1000 * 60);
-    Firebase.setwriteSizeLimit(fbdo, "small");
-    Serial.println("------------------------------------");
-    Serial.println("Firebase Connected...");
-
-  } else {
-    wifiConnected();
+    otaServerStart();
+    connecToFirebase();
+  }
+  else
+  {
+    wifiLED();
     Serial.println("WiFi Connected!");
-
-    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-    Firebase.reconnectWiFi(true);
-    Firebase.setReadTimeout(fbdo, 1000 * 60);
-    Firebase.setwriteSizeLimit(fbdo, "small");
-    Serial.println("------------------------------------");
-    Serial.println("Firebase Connected...");
+    otaServerStart();
+    connecToFirebase();
   }
 }
 
-void loop() {
+void loop()
+{
   rst_millis = millis();
-  while (digitalRead(WiFi_rst) == LOW) {
-    if (millis() - rst_millis >= 3000) {
+  while (digitalRead(WiFi_rst) == LOW)
+  {
+    if (millis() - rst_millis >= 3000)
+    {
       Serial.println("Reseting the WiFi credentials");
       writeStringToFlash("", 0);
       writeStringToFlash("", 40);
@@ -158,98 +164,128 @@ void loop() {
     }
   }
 
-  if (Firebase.ready()) {
-    databaseConnected();
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light1")) {
+  if (Firebase.ready())
+  {
+    databaseLED();
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light1"))
+    {
       Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean") {
-        light1 = fbdo.boolData();
-        Serial.println(light1);
-        if (light1 == true) {
-          digitalWrite(LED1, LOW);
-        } else {
-          digitalWrite(LED1, HIGH);
-        }
+      if (fbdo.dataType() == "boolean")
+      {
+        switch1 = fbdo.boolData();
+        switchState(switch1, PIN1);
       }
     }
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light2")) {
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light2"))
+    {
       Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean") {
-        light2 = fbdo.boolData();
-        Serial.println(light2);
-        if (light2 == true) {
-          digitalWrite(LED2, LOW);
-        } else {
-          digitalWrite(LED2, HIGH);
-        }
+      if (fbdo.dataType() == "boolean")
+      {
+        switch2 = fbdo.boolData();
+        switchState(switch2, PIN2);
       }
     }
 
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light3")) {
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light3"))
+    {
       Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean") {
-        light3 = fbdo.boolData();
-        Serial.println(light3);
-        if (light3 == true) {
-          digitalWrite(LED3, LOW);
-        } else {
-          digitalWrite(LED3, HIGH);
-        }
+      if (fbdo.dataType() == "boolean")
+      {
+        switch3 = fbdo.boolData();
+        switchState(switch3, PIN3);
       }
     }
 
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light4")) {
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light4"))
+    {
       Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean") {
-        light4 = fbdo.boolData();
-        Serial.println(light4);
-        if (light4 == true) {
-          digitalWrite(LED4, LOW);
-        } else {
-          digitalWrite(LED4, HIGH);
-        }
+      if (fbdo.dataType() == "boolean")
+      {
+        switch4 = fbdo.boolData();
+        switchState(switch4, PIN4);
       }
     }
     delay(1000);
-  } else {
+  }
+  else
+  {
     errorLED();
     Serial.println(fbdo.errorReason());
   }
 }
 
-
-void writeStringToFlash(const char* toStore, int startAddr) {
+void writeStringToFlash(const char *toStore, int startAddr)
+{
   int i = 0;
-  for (; i < LENGTH(toStore); i++) {
+  for (; i < LENGTH(toStore); i++)
+  {
     EEPROM.write(startAddr + i, toStore[i]);
   }
   EEPROM.write(startAddr + i, '\0');
   EEPROM.commit();
 }
 
-
-String readStringFromFlash(int startAddr) {
+String readStringFromFlash(int startAddr)
+{
   char in[128];
   int i = 0;
-  for (; i < 128; i++) {
+  for (; i < 128; i++)
+  {
     in[i] = EEPROM.read(startAddr + i);
   }
   return String(in);
 }
-void wifiConnected() {
-  digitalWrite(LED_R, LOW);
-  digitalWrite(LED_G, LOW);
-  digitalWrite(LED_Y, HIGH);
+
+void connecToFirebase()
+{
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  Firebase.setReadTimeout(fbdo, 1000 * 60);
+  Firebase.setwriteSizeLimit(fbdo, "small");
+  Serial.println("------------------------------------");
+  Serial.println("Firebase Connected...");
 }
 
-void databaseConnected() {
-  digitalWrite(LED_R, LOW);
-  digitalWrite(LED_Y, LOW);
-  digitalWrite(LED_G, HIGH);
+void otaServerStart()
+{
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "Hello! Welcome to my SmartHome."); });
+
+  AsyncElegantOTA.begin(&server); // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
-void errorLED() {
-  digitalWrite(LED_G, LOW);
-  digitalWrite(LED_Y, LOW);
-  digitalWrite(LED_R, HIGH);
+void wifiLED()
+{
+  digitalWrite(PIN_R, LOW);
+  digitalWrite(PIN_G, LOW);
+  digitalWrite(PIN_Y, HIGH);
+}
+
+void databaseLED()
+{
+  digitalWrite(PIN_R, LOW);
+  digitalWrite(PIN_Y, LOW);
+  digitalWrite(PIN_G, HIGH);
+}
+
+void errorLED()
+{
+  digitalWrite(PIN_G, LOW);
+  digitalWrite(PIN_Y, LOW);
+  digitalWrite(PIN_R, HIGH);
+}
+
+void switchState(bool switchStats, int switchPin)
+{
+  Serial.println(switchStats);
+  if (switchStats == true)
+  {
+    digitalWrite(switchPin, LOW);
+  }
+  else
+  {
+    digitalWrite(switchPin, HIGH);
+  }
 }
