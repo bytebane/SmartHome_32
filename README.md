cd433267cfb1a044a9ec45863b35ca5c700cf08d
ByteBane <81875670+bytebane@users.noreply.github.com>
2022-07-20 21:37:12 +0530

    device stats,etc

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# SmartHome_32
+# Filati_32
 
-# Project continued in [Filati_32](https://github.com/R3WorkZ/Filati_32.git)
 ## SmartHome Starter-Kit
 
 - ESP32
 - Arduino/C++
+
 
 ## [AsyncElegantOTA](https://github.com/ayushsharma82/AsyncElegantOTA) was used for OTA updates, to include the required libraries, follow these steps:
 
@@ -15,6 +15,16 @@
 
 Open your browser and go to __http://<ESP32_IPAddress>/update__, to Update OTA.
 
-### NOTE: -- I am using Solid State Relay where input HIGH=OFF and LOW=ON
 
-Add Manual Switch -- With normal 5v Relay, the simplest way to add manual switch would be to add a two way switch connect one end to NO and the other end to NC terminal of the relay. This way all the appliances can be manually controlled even in case of any errors, short-circuits or power failures in the Microcontroller or relay.
+
+## Other Used Libraries:
+
+- [FirebaseESP32](https://github.com/mobizt/Firebase-ESP32.git)
+- [ESPDateTime](https://github.com/mcxiaoke/ESPDateTime.git)
+
+## Boards
+- [ESP32-Espressif](https://github.com/espressif/arduino-esp32.git)
+   - ESP32-Dev-Module 
+### NOTE: -- Using Solid State Relay(4Channel) active-low, where input HIGH=OFF and LOW=ON
+
+Add Manual Switch -- With normal 5v Relay, the simplest way to add manual switch would be to add a three-way switch, connect one end to NO and the other end to NC terminal of the relay. This way all the appliances can be manually controlled even in case of any errors, short-circuits or power failures in the Microcontroller or relay.
--- SmartHome_32.ino
+++ SmartHome_32.ino
@@ -2,41 +2,45 @@
 
 #include "WiFi.h"
 #include "EEPROM.h"
-#include "FirebaseESP32.h"
 #include "AsyncTCP.h"
-#include "ESPAsyncWebServer.h"
+#include "ESPDateTime.h"
+#include "FirebaseESP32.h"
 #include "AsyncElegantOTA.h"
+#include "ESPAsyncWebServer.h"
+#include "my_credentials.h"  //Remove me
 
 #define LENGTH(x) (strlen(x) + 1)
 #define EEPROM_SIZE 200
 #define WiFi_rst 0
-#define FIREBASE_HOST "YOUR_RTDBLink"
-#define FIREBASE_AUTH "YOUR_AUTH_KEY"
 
-String ssid;
-String pass;
-unsigned long rst_millis;
+#define PIN_R 21
+#define PIN_Y 22
+#define PIN_G 23
 
-FirebaseData fbdo;
+#define PIN1 16
+#define PIN2 17
+#define PIN3 18
+#define PIN4 19
+
+// #define FIREBASE_HOST "UnComment_Me&add__YOUR_RTDBLink"
+// #define FIREBASE_AUTH "UnComment_Me&add__YOUR_AUTH_KEY"
 
 AsyncWebServer server(80);
 
-int PIN_R = 21;
-int PIN_Y = 22;
-int PIN_G = 23;
+int LOOP_Counter = 0;
 
-int PIN1 = 16;
-int PIN2 = 17;
-int PIN3 = 18;
-int PIN4 = 19;
+String ssid;
+String pass;
+FirebaseData fbdo;
+
+unsigned long rst_millis;
 
 bool switch1;
 bool switch2;
 bool switch3;
 bool switch4;
 
-void setup()
-{
+void setup() {
 
   Serial.begin(115200);
 
@@ -55,40 +59,35 @@ void setup()
   digitalWrite(PIN_G, HIGH);
   digitalWrite(PIN_Y, HIGH);
 
-  // For Solid State Relay High=OFF
+  // Solid State Relay Solution for LOW OnInit
   digitalWrite(PIN1, HIGH);
   digitalWrite(PIN2, HIGH);
   digitalWrite(PIN3, HIGH);
   digitalWrite(PIN4, HIGH);
 
-  if (!EEPROM.begin(EEPROM_SIZE))
-  {
-    Serial.println("failed to init EEPROM");
+  if (!EEPROM.begin(EEPROM_SIZE)) {
+    Serial.println("Failed to Init EEPROM");
     delay(1000);
-  }
-  else
-  {
+  } else {
     ssid = readStringFromFlash(0);
-    Serial.print("SSID = ");
-    Serial.println(ssid);
+    // Serial.print("SSID = ");
+    // Serial.println(ssid);
     pass = readStringFromFlash(40);
-    Serial.print("pass = ");
-    Serial.println(pass);
+    // Serial.print("pass = ");
+    // Serial.println(pass);
   }
 
   WiFi.begin(ssid.c_str(), pass.c_str());
 
   delay(3500);
 
-  if (WiFi.status() != WL_CONNECTED)
-  {
+  if (WiFi.status() != WL_CONNECTED) {
 
     WiFi.mode(WIFI_AP_STA);
     WiFi.beginSmartConfig();
 
     Serial.println("Waiting for SmartConfig.");
-    while (!WiFi.smartConfigDone())
-    {
+    while (!WiFi.smartConfigDone()) {
       digitalWrite(PIN_G, LOW);
       digitalWrite(PIN_Y, LOW);
       Serial.print(".");
@@ -115,8 +114,7 @@ void setup()
     Serial.println("SmartConfig received.");
 
     Serial.println("Waiting for WiFi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
+    while (WiFi.status() != WL_CONNECTED) {
       Serial.print(".");
     }
     wifiLED();
@@ -127,33 +125,31 @@ void setup()
 
     ssid = WiFi.SSID();
     pass = WiFi.psk();
-    Serial.print("SSID:");
-    Serial.println(ssid);
-    Serial.print("pass:");
-    Serial.println(pass);
+    // Serial.print("SSID:");
+    // Serial.println(ssid);
+    // Serial.print("pass:");
+    // Serial.println(pass);
     Serial.println("Store SSID & pass in Flash");
     writeStringToFlash(ssid.c_str(), 0);
     writeStringToFlash(pass.c_str(), 40);
 
+    setupDateTime();
     otaServerStart();
     connecToFirebase();
-  }
-  else
-  {
+  } else {
     wifiLED();
     Serial.println("WiFi Connected!");
+
+    setupDateTime();
     otaServerStart();
     connecToFirebase();
   }
 }
 
-void loop()
-{
+void loop() {
   rst_millis = millis();
-  while (digitalRead(WiFi_rst) == LOW)
-  {
-    if (millis() - rst_millis >= 3000)
-    {
+  while (digitalRead(WiFi_rst) == LOW) {
+    if (millis() - rst_millis >= 3000) {
       Serial.println("Reseting the WiFi credentials");
       writeStringToFlash("", 0);
       writeStringToFlash("", 40);
@@ -164,80 +160,76 @@ void loop()
     }
   }
 
-  if (Firebase.ready())
-  {
+  if (Firebase.ready()) {
     databaseLED();
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light1"))
-    {
-      Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean")
-      {
+    LOOP_Counter++;
+    if (LOOP_Counter >= 10) {
+      Serial.println(Firebase.RTDB.setString(&fbdo, "/myHome/deviceStats/dateTime", DateTime.toString().c_str()) ? "DateTime Sent to RTDB" : fbdo.errorReason().c_str());
+      LOOP_Counter = 0;
+    }
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/switches/switch1")) {
+      // Serial.println(fbdo.dataType());
+      if (fbdo.dataType() == "boolean") {
         switch1 = fbdo.boolData();
         switchState(switch1, PIN1);
       }
     }
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light2"))
-    {
-      Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean")
-      {
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/switches/switch2")) {
+      if (fbdo.dataType() == "boolean") {
         switch2 = fbdo.boolData();
         switchState(switch2, PIN2);
       }
     }
 
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light3"))
-    {
-      Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean")
-      {
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/switches/switch3")) {
+      if (fbdo.dataType() == "boolean") {
         switch3 = fbdo.boolData();
         switchState(switch3, PIN3);
       }
     }
 
-    if (Firebase.RTDB.getBool(&fbdo, "/myHome/light4"))
-    {
-      Serial.println(fbdo.dataType());
-      if (fbdo.dataType() == "boolean")
-      {
+    if (Firebase.RTDB.getBool(&fbdo, "/myHome/switches/switch4")) {
+      if (fbdo.dataType() == "boolean") {
         switch4 = fbdo.boolData();
         switchState(switch4, PIN4);
       }
     }
-    delay(1000);
-  }
-  else
-  {
+    // delay(1000); //Better without delay
+  } else {
     errorLED();
     Serial.println(fbdo.errorReason());
   }
 }
 
-void writeStringToFlash(const char *toStore, int startAddr)
-{
+void writeStringToFlash(const char *toStore, int startAddr) {
   int i = 0;
-  for (; i < LENGTH(toStore); i++)
-  {
+  for (; i < LENGTH(toStore); i++) {
     EEPROM.write(startAddr + i, toStore[i]);
   }
   EEPROM.write(startAddr + i, '\0');
   EEPROM.commit();
 }
 
-String readStringFromFlash(int startAddr)
-{
+String readStringFromFlash(int startAddr) {
   char in[128];
   int i = 0;
-  for (; i < 128; i++)
-  {
+  for (; i < 128; i++) {
     in[i] = EEPROM.read(startAddr + i);
   }
   return String(in);
 }
 
-void connecToFirebase()
-{
+void setupDateTime() {
+  DateTime.setServer("asia.pool.ntp.org");
+  DateTime.setTimeZone("IST-5:30");
+  DateTime.begin();
+  if (!DateTime.isTimeValid()) {
+    Serial.println("Failed to get time from server.");
+  }
+}
+
+
+void connecToFirebase() {
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
   Firebase.setReadTimeout(fbdo, 1000 * 60);
@@ -246,46 +238,44 @@ void connecToFirebase()
   Serial.println("Firebase Connected...");
 }
 
-void otaServerStart()
-{
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(200, "text/plain", "Hello! Welcome to my SmartHome."); });
+void otaServerStart() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hello! Welcome to MyHome A SmartHome Assistant.");
+  });
 
-  AsyncElegantOTA.begin(&server); // Start ElegantOTA
+  AsyncElegantOTA.begin(&server);  // Start ElegantOTA
   server.begin();
-  Serial.println("HTTP server started");
+  Serial.println("OTA server started.");
 }
 
-void wifiLED()
-{
+void wifiLED() {
   digitalWrite(PIN_R, LOW);
   digitalWrite(PIN_G, LOW);
   digitalWrite(PIN_Y, HIGH);
 }
 
-void databaseLED()
-{
+void databaseLED() {
   digitalWrite(PIN_R, LOW);
   digitalWrite(PIN_Y, LOW);
   digitalWrite(PIN_G, HIGH);
 }
 
-void errorLED()
-{
+void errorLED() {
   digitalWrite(PIN_G, LOW);
   digitalWrite(PIN_Y, LOW);
   digitalWrite(PIN_R, HIGH);
 }
 
-void switchState(bool switchStats, int switchPin)
-{
-  Serial.println(switchStats);
-  if (switchStats == true)
-  {
+void switchState(bool switchStats, int switchPin) {
+  if (switchStats == true) {
     digitalWrite(switchPin, LOW);
-  }
-  else
-  {
+  } else {
     digitalWrite(switchPin, HIGH);
   }
+  String isOn = switchStats ? "On" : "Off";
+  String sName = switchPin == 16 ? "Switch1" : switchPin == 17 ? "Switch2"
+                                             : switchPin == 18 ? "Switch3"
+                                             : switchPin == 19 ? "Switch4"
+                                                               : "something went wrong";
+  Serial.println(sName + "\t:-\t" + isOn);
 }
